1eeaf5e01f2d206535c164076a0871d9167c7d6d
Rachel Law <rlaw1294@gmail.com>
2016-03-18 00:20:36 -0700

    switch to main comp

--- lab6/RachelLawLab6_2.cpp
+++ lab6/RachelLawLab6_2.cpp
@@ -1,15 +1,19 @@
 // Rachel Law (861071722)
 
 #include <iostream>
-#include "cpp.h"
 #include <string.h>
 #include <math.h>
 #include <vector>
 #include <fstream>
 #include <stdlib.h>
+#include <time.h>
 using namespace std;
 
-#define MAX_SAMPLE_SIZE 10
+#define LEFT_TRIANGLE 0
+#define RIGHT_TRIANGLE 1
+#define UNIFORM_DIST 2
+
+#define MAX_SAMPLE_SIZE 25
 double *samples;
 double *left_tri_results;
 double *right_tri_results;
@@ -25,7 +29,7 @@ void DoPDF(double *samples, double *results, int samples_size, double A, double 
 	// Get Samples
 	for (int i=0; i<samples_size; i++) {
 		//samples[i] = i;
-		samples[i] = uniform(0,1);
+		//samples[i] = uniform(0,1);
 
 		results[i] = PDFFunc(A, samples[i]);
 	}
@@ -47,8 +51,29 @@ void PrintCSV(vector<double> &v) {
 	myfile << ", ";
 }
 
-extern "C" void sim()
+
+void MakeSamples(double *ar, int ar_size, double A, int sample_type) {
+	for (int i = 0; i<ar_size; i++) {
+		double U = (rand() % 100000)/100000.0;
+		if (sample_type == LEFT_TRIANGLE) ar[i] = sqrt(U*A*A);
+		else if (sample_type == RIGHT_TRIANGLE) ar[i] = sqrt((1-U)*A*A);
+		else if (sample_type == UNIFORM_DIST) ar[i] = U*A;
+	}
+} 
+
+int main()
 {
+	srand(time(NULL));
+	samples = new double [MAX_SAMPLE_SIZE];
+	left_tri_results = new double [MAX_SAMPLE_SIZE];
+	right_tri_results = new double [MAX_SAMPLE_SIZE];
+	uniform_results = new double [MAX_SAMPLE_SIZE];
+
+	MakeSamples(samples, MAX_SAMPLE_SIZE, 10, LEFT_TRIANGLE);
+	PrintArray(samples, MAX_SAMPLE_SIZE);
+
+	
+	/*
 	// Edit this vector for all sample sizes you want to try
 	vector<int> samples_sizes; 
 	samples_sizes.push_back(MAX_SAMPLE_SIZE);
@@ -70,4 +95,5 @@ extern "C" void sim()
 	PrintArray(left_tri_results, MAX_SAMPLE_SIZE);
 	//PrintArray(right_tri_results, MAX_SAMPLE_SIZE);
 	//PrintArray(uniform_results, MAX_SAMPLE_SIZE);
+	*/
 }
